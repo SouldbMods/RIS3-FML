b4410aa231926cc829f0ae1b47533b05d811c125
SouldbMods <teamreallyinspace@gmail.com>
2021-04-26 16:41:19 +0530

    Added oxygen effect

---
+++ src/main/java/xyz/souldb/ris3/potion/OxygenPotion.java
@@ -0,0 +1,72 @@
+
+package xyz.souldb.ris3.potion;
+
+import xyz.souldb.ris3.Ris3ModElements;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.potion.EffectType;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effect;
+
+@Ris3ModElements.ModElement.Tag
+public class OxygenPotion extends Ris3ModElements.ModElement {
+	@ObjectHolder("ris3:oxygen")
+	public static final Effect potion = null;
+	public OxygenPotion(Ris3ModElements instance) {
+		super(instance, 152);
+		FMLJavaModLoadingContext.get().getModEventBus().register(this);
+	}
+
+	@SubscribeEvent
+	public void registerEffect(RegistryEvent.Register<Effect> event) {
+		event.getRegistry().register(new EffectCustom());
+	}
+	public static class EffectCustom extends Effect {
+		private final ResourceLocation potionIcon;
+		public EffectCustom() {
+			super(EffectType.BENEFICIAL, -10066177);
+			setRegistryName("oxygen");
+			potionIcon = new ResourceLocation("ris3:textures/oxygenicon.png");
+		}
+
+		@Override
+		public String getName() {
+			return "effect.oxygen";
+		}
+
+		@Override
+		public boolean isBeneficial() {
+			return true;
+		}
+
+		@Override
+		public boolean isInstant() {
+			return false;
+		}
+
+		@Override
+		public boolean shouldRenderInvText(EffectInstance effect) {
+			return false;
+		}
+
+		@Override
+		public boolean shouldRender(EffectInstance effect) {
+			return false;
+		}
+
+		@Override
+		public boolean shouldRenderHUD(EffectInstance effect) {
+			return false;
+		}
+
+		@Override
+		public boolean isReady(int duration, int amplifier) {
+			return true;
+		}
+	}
+}

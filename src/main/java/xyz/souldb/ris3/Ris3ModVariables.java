8732be7a65c11ed990c57896d7bd795e5a1240df
SouldbMods <teamreallyinspace@gmail.com>
2021-05-04 09:53:26 +0530

    Added oxygen varibles

--- src/main/java/xyz/souldb/ris3/Ris3ModVariables.java
+++ src/main/java/xyz/souldb/ris3/Ris3ModVariables.java
@@ -2,17 +2,32 @@ package xyz.souldb.ris3;
 
 import net.minecraftforge.fml.network.PacketDistributor;
 import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.common.util.FakePlayer;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.common.capabilities.Capability;
 
 import net.minecraft.world.storage.WorldSavedData;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.IServerWorld;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Direction;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.nbt.INBT;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.Minecraft;
 
 import java.util.function.Supplier;
 
@@ -20,6 +35,13 @@ public class Ris3ModVariables {
 	public Ris3ModVariables(Ris3ModElements elements) {
 		elements.addNetworkMessage(WorldSavedDataSyncMessage.class, WorldSavedDataSyncMessage::buffer, WorldSavedDataSyncMessage::new,
 				WorldSavedDataSyncMessage::handler);
+		elements.addNetworkMessage(PlayerVariablesSyncMessage.class, PlayerVariablesSyncMessage::buffer, PlayerVariablesSyncMessage::new,
+				PlayerVariablesSyncMessage::handler);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
+	}
+
+	private void init(FMLCommonSetupEvent event) {
+		CapabilityManager.INSTANCE.register(PlayerVariables.class, new PlayerVariablesStorage(), PlayerVariables::new);
 	}
 
 	@SubscribeEvent
@@ -85,7 +107,7 @@ public class Ris3ModVariables {
 		public boolean EARTHORBIT = false;
 		public boolean MARSORBIT = false;
 		public double RocketGUIState = 0;
-		public double y = 0;
+		public double y = 0.0;
 		public MapVariables() {
 			super(DATA_NAME);
 		}
@@ -154,6 +176,117 @@ public class Ris3ModVariables {
 						MapVariables.clientSide = (MapVariables) message.data;
 					else
 						WorldVariables.clientSide = (WorldVariables) message.data;
+				}
+			});
+			context.setPacketHandled(true);
+		}
+	}
+	@CapabilityInject(PlayerVariables.class)
+	public static Capability<PlayerVariables> PLAYER_VARIABLES_CAPABILITY = null;
+	@SubscribeEvent
+	public void onAttachCapabilities(AttachCapabilitiesEvent<Entity> event) {
+		if (event.getObject() instanceof PlayerEntity && !(event.getObject() instanceof FakePlayer))
+			event.addCapability(new ResourceLocation("ris3", "player_variables"), new PlayerVariablesProvider());
+	}
+	private static class PlayerVariablesProvider implements ICapabilitySerializable<INBT> {
+		private final LazyOptional<PlayerVariables> instance = LazyOptional.of(PLAYER_VARIABLES_CAPABILITY::getDefaultInstance);
+		@Override
+		public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
+			return cap == PLAYER_VARIABLES_CAPABILITY ? instance.cast() : LazyOptional.empty();
+		}
+
+		@Override
+		public INBT serializeNBT() {
+			return PLAYER_VARIABLES_CAPABILITY.getStorage().writeNBT(PLAYER_VARIABLES_CAPABILITY, this.instance.orElseThrow(RuntimeException::new),
+					null);
+		}
+
+		@Override
+		public void deserializeNBT(INBT nbt) {
+			PLAYER_VARIABLES_CAPABILITY.getStorage().readNBT(PLAYER_VARIABLES_CAPABILITY, this.instance.orElseThrow(RuntimeException::new), null,
+					nbt);
+		}
+	}
+
+	private static class PlayerVariablesStorage implements Capability.IStorage<PlayerVariables> {
+		@Override
+		public INBT writeNBT(Capability<PlayerVariables> capability, PlayerVariables instance, Direction side) {
+			CompoundNBT nbt = new CompoundNBT();
+			nbt.putDouble("oxygen_tank_1_oxygen", instance.oxygen_tank_1_oxygen);
+			nbt.putDouble("oxygen_tank_2_oxygen", instance.oxygen_tank_2_oxygen);
+			return nbt;
+		}
+
+		@Override
+		public void readNBT(Capability<PlayerVariables> capability, PlayerVariables instance, Direction side, INBT inbt) {
+			CompoundNBT nbt = (CompoundNBT) inbt;
+			instance.oxygen_tank_1_oxygen = nbt.getDouble("oxygen_tank_1_oxygen");
+			instance.oxygen_tank_2_oxygen = nbt.getDouble("oxygen_tank_2_oxygen");
+		}
+	}
+
+	public static class PlayerVariables {
+		public double oxygen_tank_1_oxygen = 0.0;
+		public double oxygen_tank_2_oxygen = 0.0;
+		public void syncPlayerVariables(Entity entity) {
+			if (entity instanceof ServerPlayerEntity)
+				Ris3Mod.PACKET_HANDLER.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) entity), new PlayerVariablesSyncMessage(this));
+		}
+	}
+	@SubscribeEvent
+	public void onPlayerLoggedInSyncPlayerVariables(PlayerEvent.PlayerLoggedInEvent event) {
+		if (!event.getPlayer().world.isRemote())
+			((PlayerVariables) event.getPlayer().getCapability(PLAYER_VARIABLES_CAPABILITY, null).orElse(new PlayerVariables()))
+					.syncPlayerVariables(event.getPlayer());
+	}
+
+	@SubscribeEvent
+	public void onPlayerRespawnedSyncPlayerVariables(PlayerEvent.PlayerRespawnEvent event) {
+		if (!event.getPlayer().world.isRemote())
+			((PlayerVariables) event.getPlayer().getCapability(PLAYER_VARIABLES_CAPABILITY, null).orElse(new PlayerVariables()))
+					.syncPlayerVariables(event.getPlayer());
+	}
+
+	@SubscribeEvent
+	public void onPlayerChangedDimensionSyncPlayerVariables(PlayerEvent.PlayerChangedDimensionEvent event) {
+		if (!event.getPlayer().world.isRemote())
+			((PlayerVariables) event.getPlayer().getCapability(PLAYER_VARIABLES_CAPABILITY, null).orElse(new PlayerVariables()))
+					.syncPlayerVariables(event.getPlayer());
+	}
+
+	@SubscribeEvent
+	public void clonePlayer(PlayerEvent.Clone event) {
+		PlayerVariables original = ((PlayerVariables) event.getOriginal().getCapability(PLAYER_VARIABLES_CAPABILITY, null)
+				.orElse(new PlayerVariables()));
+		PlayerVariables clone = ((PlayerVariables) event.getEntity().getCapability(PLAYER_VARIABLES_CAPABILITY, null).orElse(new PlayerVariables()));
+		if (!event.isWasDeath()) {
+			clone.oxygen_tank_1_oxygen = original.oxygen_tank_1_oxygen;
+			clone.oxygen_tank_2_oxygen = original.oxygen_tank_2_oxygen;
+		}
+	}
+	public static class PlayerVariablesSyncMessage {
+		public PlayerVariables data;
+		public PlayerVariablesSyncMessage(PacketBuffer buffer) {
+			this.data = new PlayerVariables();
+			new PlayerVariablesStorage().readNBT(null, this.data, null, buffer.readCompoundTag());
+		}
+
+		public PlayerVariablesSyncMessage(PlayerVariables data) {
+			this.data = data;
+		}
+
+		public static void buffer(PlayerVariablesSyncMessage message, PacketBuffer buffer) {
+			buffer.writeCompoundTag((CompoundNBT) new PlayerVariablesStorage().writeNBT(null, message.data, null));
+		}
+
+		public static void handler(PlayerVariablesSyncMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
+			NetworkEvent.Context context = contextSupplier.get();
+			context.enqueueWork(() -> {
+				if (!context.getDirection().getReceptionSide().isServer()) {
+					PlayerVariables variables = ((PlayerVariables) Minecraft.getInstance().player.getCapability(PLAYER_VARIABLES_CAPABILITY, null)
+							.orElse(new PlayerVariables()));
+					variables.oxygen_tank_1_oxygen = message.data.oxygen_tank_1_oxygen;
+					variables.oxygen_tank_2_oxygen = message.data.oxygen_tank_2_oxygen;
 				}
 			});
 			context.setPacketHandled(true);
